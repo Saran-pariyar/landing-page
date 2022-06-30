65f0fb60d68f3fd2dfc92bf32be865176413caa8
Saran-pariyar <spariyar05@gmail.com>
2022-06-30 16:20:38 +0545

    Made navbar with responsiveness

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './assets/css/App.css';
+import Navbar from './components/Navbar/Navbar';
+import HeroSection from './components/HeroSection/HeroSection';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      d
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HeroSection />} />
+        <Route path='/landing-page' element={<HeroSection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <nav className="navbar">
+      <div className="logo">Logo</div>
+      <div
+        className="ham-icon"
+        onClick={() => (showMenu ? setShowMenu(false) : setShowMenu(true))}
+      >
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+      <ul className={`nav-list ${showMenu ? "showMenu" : ""}`}>
+        <li>
+          <NavLink
+            to="/"
+            className="nav-link"
+            onClick={() => setShowMenu(false)}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className="nav-link"
+            onClick={() => setShowMenu(false)}
+          >
+            About Us
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/contact"
+            className="nav-link"
+            onClick={() => setShowMenu(false)}
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
